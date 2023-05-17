43ad3c3fa51f09d7d66ef3ecd2bb593d40ef5fee
Alesya Makarevich <makariki0501@gmail.com>
2023-05-17 22:06:47 +0300

    H1

---
+++ homework1/js/main.js
@@ -0,0 +1,72 @@
+'use strict'
+
+// //Task 1
+//
+// let x = 20
+// let y = 58
+// let z = 42
+// console.log(x+y+z)
+//
+// //Task 2
+//
+// let min = 60
+// let h = 60
+// let d = 24
+// let y = 365
+// let myAgeInSeconds = 21*min*h*d*y
+// console.log(myAgeInSeconds)
+//
+// //Task 3
+//
+// let count = 42
+// let userName = '42'
+//
+// // 1 способ
+// count = String(count)
+// userName = Number(userName)
+// console.log(typeof count)
+// console.log(typeof userName)
+//
+// // 2 способ
+// count ='{$count}';
+// console.log(typeof count)
+// userName = +userName
+// console.log(typeof userName)
+//
+// //Task 4
+//
+// let a = 1
+// let b = 2
+// let c = "белых медведей"
+// a = String(a)
+// b = String(b)
+// let d = a+b+' '+c
+// console.log(d)
+//
+// //Task 5
+//
+// let w1 = 'доступ'
+// let w2 = 'морпех'
+// let w3 = 'наледь'
+// let w4 = 'поперек'
+// let w5 = 'рубило'
+//
+// let lengthWords = (w1+w2+w3+w4+w5).length
+// console.log(lengthWords)
+//
+// //Task 6
+//
+// let num = 587
+// let str = 'hello'
+// let bool = Boolean(true)
+//
+// console.log(`Variable: num have type: ${typeof num}
+// Variable: str have type: ${typeof str}
+// Variable: bool have type: ${typeof bool}`)
+
+// //Task 7
+//
+// let name=prompt("Say your name:")
+// let age=prompt("Say your age:")
+//
+// console.log(`Name: ${name}, age: ${age}`)
